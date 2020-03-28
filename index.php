13babaa892d01ed9a22d5f54b8b444759e2fb55d
Emily Wallace - Price <emily.wallace-price@technologywithin.com>
2020-03-28 19:55:41 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include "includes/header.php"; ?>
+
+<body>
+<?php include "includes/navbar.php"; ?>
+
+
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+		<?php include "includes/content.php"; ?>
+
+
+		<?php include "includes/sidebar.php"; ?>
+
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+	<?php include "includes/footer.php"; ?>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
