540b167de36f06ba364b3d9f9dcb8a2a31fe5266
Emily Wallace - Price <emily.wallace-price@technologywithin.com>
2020-05-04 12:28:15 +0100

    profile edit created and select boxes for roles updated

--- admin/includes/edit_profile.php
+++ admin/includes/edit_profile.php
@@ -1,46 +1,50 @@
+<?php //include "includes/admin_header.php" ?>
 <?php
-    if (isset($_GET['u_id'])) {
-        $the_user_id = $_GET['u_id'];
 
-        $query = "SELECT * fROM users WHERE id = $the_user_id";
-        $edit_user_query = mysqli_query($connection, $query);
+    if(isset($_SESSION['username'])) {
 
-        while ($row = mysqli_fetch_assoc($edit_user_query)) {
+        $username = $_SESSION['username'];
+
+        $query = "SELECT * FROM users WHERE username = '{$username}' ";
+
+        $select_user_profile_query = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_array($select_user_profile_query)) {
+
+            $user_id = $row['id'];
             $username = $row['username'];
+            $user_password= $row['user_password'];
             $user_firstname = $row['user_firstname'];
             $user_lastname = $row['user_lastname'];
             $user_email = $row['user_email'];
-            $user_role = $row['user_role'];
             $user_image = $row['user_image'];
+            $user_role= $row['user_role'];
         }
-
-        if (isset($_POST['update_user'])) {
-        	$username = $_POST['username'];
+        if (isset($_POST['update_profile'])) {
+            $username = $_POST['username'];
             $user_firstname = $_POST['user_firstname'];
             $user_lastname = $_POST['user_lastname'];
             $user_email = $_POST['user_email'];
             $user_role = $_POST['user_role'];
-
             $user_image =  $_FILES['user_image'] ['name'];
             $user_image_temp = $_FILES['user_image'] ['tmp_name'];
             $location = "../images/";
             move_uploaded_file($user_image_temp, $location.$user_image);
-
             if(empty($user_image)) {
-                $query = "SELECT * FROM users WHERE id = $the_user_id ";
+                $query = "SELECT * FROM users WHERE username = $username ";
                 $select_image = mysqli_query($connection,$query);
 
                 while($row = mysqli_fetch_array($select_image)) {
                     $user_image = $row['user_image'];
                 }
             }
-            $query = "UPDATE users SET username = ' {$username} ', user_firstname = '{$user_firstname}',user_lastname = ' {$user_lastname}',user_email = ' {$user_email}', user_role = ' {$user_role} ', user_image = '{$user_image}'  WHERE id = {$the_user_id}  ";
-            $update_post = mysqli_query($connection, $query);
-            confirmSubmit($update_post);
-            header("Location: users.php"); exit;
+            $query = "UPDATE users SET username = '{$username} ', user_firstname = '{$user_firstname}',user_lastname = ' {$user_lastname}',user_email = ' {$user_email}', user_role = ' {$user_role} ', user_image = '{$user_image}' WHERE username = '{$username}'  ";
+            $update_user = mysqli_query($connection, $query);
+            confirmSubmit($update_user);
+            header("Location: profile.php"); exit;
         }
     }
-    ?>
+?>
 <form action="" method="post" enctype="multipart/form-data">
     <div class="col-md-8">
         <div class="form-group ">
@@ -58,18 +62,27 @@
         <div class="form-group">
             <label for="user_email">Email</label>
             <textarea name="user_email" class="form-control"><?php echo $user_email ?></textarea>
+
         </div>
         <div class="form-group">
             <label for="user_role">Role</label>
 			<select name="user_role" id="" class="form-control">
+
 				<option value="<?php echo $user_role; ?>"><?php echo $user_role; ?></option>
                 <?php
+
                     if($user_role == 'admin') {
+
                         echo "<option value='subscriber'>subscriber</option>";
+
                     } else {
+
                         echo "<option value='admin'>admin</option>";
+
                     }
+
                 ?>
+
 			</select>
         </div>
         <div class="form-group">
@@ -81,9 +94,12 @@
             max-width:100px;' >
             <input type="file" name="user_image" class="form-control" value="<?php echo $user_image ?>">
         </div>
-
+		<div class="form-group">
+			<label for="user_password">Password</label>
+			<input type="password" name="user_password" class="form-control" value="<?php echo $user_password ?>">
+		</div>
         <div class="form-group">
-            <input type="submit" class="btn btn-primary" name="update_user" value="Update User">
+            <input type="submit" class="btn btn-primary" name="update_profile" value="Update Profile">
         </div>
     </div>
 </form>
---
+++ admin/includes/view_profile.php
@@ -0,0 +1,5 @@
+<?php
+    if(isset($_SESSION['username'])) {
+        echo $_SESSION['username'];
+    }
+?>
