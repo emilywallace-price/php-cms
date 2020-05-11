6d0cb706cb7426f13991b52a8868e93beb76ddf4
Emily Wallace - Price <emily.wallace-price@technologywithin.com>
2020-05-11 10:56:22 +0100

    refactor the frontend links via photo and read more

--- includes/content.php
+++ includes/content.php
@@ -19,18 +19,19 @@
             <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo "$post_title" ?></a>
         </h2>
         <p class="lead">
-            by <a href="index.php"><?php echo "$post_author" ?></a>
+            by <?php echo "$post_author" ?>
         </p>
         <p>
             <span class="glyphicon glyphicon-time"></span>
             Posted on <?php echo "$post_date" ?>
         </p>
         <hr>
-        <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id; ?>"> <img class="img-responsive" src="images/<?php echo $post_image ?>" alt=""></a>
         <hr>
         <p>
             <?php echo "$post_content" ?>
         </p>
+                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
         <hr>
     <?php } ?>
 </div>
