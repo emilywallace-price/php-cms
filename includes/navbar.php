43c611443ea4c44d8957eefe8f54a42db1d85f7a
Emily Wallace - Price <emily.wallace-price@technologywithin.com>
2020-05-18 13:20:35 +0100

    only displays admin link if admin is logged in

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,20 +1,20 @@
 <?php include "includes/db.php"; ?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/index.php">Emmie's Blog</a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="/index.php">Emmie's Blog</a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
                 <?php
                     $query = "SELECT * fROM categories";
                     $select_categories_query = mysqli_query($connection, $query);
@@ -24,12 +24,16 @@
                         echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                     }
                 ?>
-                <li>
-                    <a href="admin/index.php">Admin</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
+				<li>
+                    <?php
+                        if (isset($_SESSION['username'])) {
+                            echo '<a href="admin/index.php">Admin</a>';
+                        }
+                    ?>
+				</li>
+			</ul>
+		</div>
+		<!-- /.navbar-collapse -->
+	</div>
+	<!-- /.container -->
 </nav>
